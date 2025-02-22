022e408cb778908b87f96dd27d8b7e56065f499b
Sasriadi Bachtiar Farid <157808346+yardeee43@users.noreply.github.com>
2025-02-23 06:27:41 +0700

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg12;
+
+/**
+ *
+ * @author bachtiar
+ */
+public class Diamond {
+    public static void main(String[] args) {
+       int n = 9; // Harus Ganjil
+
+        // Bagian atas diamond
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 2; i >= 0; i--) { 
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    
+        
+       
+            
+                
+            
+            
+            
+        
+        
+        
+    }
+    
+}
